cca3bf20c819bfcd6fff43c19389b1baa33febd7
𝖣𝖺𝖽𝖽𝗒 𝖬𝖺𝗈𝗎 <daddymaouu@gmail.com>
2026-08-20 12:19:27 +0100

    fix: remove react-helmet-async dependency

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
 import Home from './pages/Home'
 import Verify from './pages/Verify'
 import Docs from './pages/Docs'
 
 export default function App() {
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/verify/:id" element={<Verify />} />
-          <Route path="/docs" element={<Docs />} />
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/verify/:id" element={<Verify />} />
+        <Route path="/docs" element={<Docs />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
